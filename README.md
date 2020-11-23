3d9dfdd808dc7f51257cb2a9ee94c818044cac09
Noam Zaks <noamzaks@outlook.com>
2020-11-23 17:51:18 +0000

    added material ui

--- README.md
+++ README.md
@@ -3,4 +3,4 @@ The project is (funny enough) not affiliated with MisCar currently.
 
 ## Dependencies
 - This is only the frontend of the application. The firebase scripts are inside another repository.
-- You will need NodeJS and React to develop this app. If you have node installed, just run npm install inside the root directory of the project.
+- You will need NodeJS and React to develop this app. If you have node installed, just run npm install inside the root directory of the project.                                
--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
 import React from 'react';
+import {createMuiTheme} from '@material-ui/core/styles';
 import './App.css';
+import ThemeProvider from "@material-ui/styles/ThemeProvider";
+
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            main: '#7e0c2b'
+        }
+    }
+})
 
 function App() {
-  return (
-    <div className="App">
-      <h1>
-        MisCar
-      </h1>
-    </div>
-  );
+    return (
+        <ThemeProvider theme={theme}>
+            <h1>
+                MisCar
+            </h1>
+        </ThemeProvider>
+    );
 }
 
 export default App;
